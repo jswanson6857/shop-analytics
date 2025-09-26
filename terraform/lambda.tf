71747d56a9817015e4c9dde53ae973c373e45d1a
Joseph <jswanson6857@gmail.com>
2025-09-25 22:18:04 -0400

    feat: Init new project

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,27 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach basic execution policy
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Create CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "webhook_handler_logs" {
   name              = "/aws/lambda/${var.project_name}-webhook-handler"
   retention_in_days = 7
