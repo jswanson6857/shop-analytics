a099bc5ca5e94efbd6e6261ceca514cd13813c6b
joseph.d.swanson <joseph.d.swanson@students.pcci.edu>
2025-09-26 19:19:22 -0400

    feat: adding UI

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,11 +11,11 @@ terraform {
       version = "~> 2.4"
     }
   }
-  backend "s3" {
-    bucket         = "webhook-terraform-state-dda36cd0"  # Replace with actual bucket name from step 3
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "webhook-terraform-locks"
+  
+  cloud {
+    organization = "webhook-ingestion-org"
+    workspaces {
+      name = "webhook-ingestion"
+    }
   }
 }
