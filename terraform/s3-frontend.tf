573b2f1bb3bc665268cb900c9718970dca84c923
joseph.d.swanson <joseph.d.swanson@students.pcci.edu>
2025-09-26 16:45:48 -0400

    feat: adding UI

--- terraform/s3-frontend.tf
+++ terraform/s3-frontend.tf
@@ -1,5 +1,5 @@
 # terraform/s3-frontend.tf
-# Simple, working S3 + CloudFront configuration
+# Working S3 + CloudFront configuration
 
 # Random suffix for unique bucket name
 resource "random_id" "bucket_suffix" {
@@ -9,19 +9,6 @@ resource "random_id" "bucket_suffix" {
 # S3 bucket for static website hosting
 resource "aws_s3_bucket" "frontend" {
   bucket = "${var.project_name}-frontend-${random_id.bucket_suffix.hex}"
-}
-
-# S3 bucket website configuration
-resource "aws_s3_bucket_website_configuration" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
 }
 
 # S3 bucket public access block - ALLOW public access for website hosting
@@ -54,13 +41,80 @@ resource "aws_s3_bucket_policy" "frontend" {
   depends_on = [aws_s3_bucket_public_access_block.frontend]
 }
 
-# Outputs - S3 website URL for direct access
+# CloudFront distribution for HTTPS access
+resource "aws_cloudfront_distribution" "frontend" {
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.frontend.bucket}"
+    
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${aws_s3_bucket.frontend.bucket}"
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  # Handle React Router routes
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-frontend"
+  }
+}
+
+# Outputs
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
   value       = aws_s3_bucket.frontend.bucket
 }
 
-output "s3_website_url" {
-  description = "S3 website URL (use this if CloudFront doesn't work)"
-  value       = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
+output "cloudfront_url" {
+  description = "CloudFront HTTPS URL (use this for your dashboard)"
+  value       = "https://${aws_cloudfront_distribution.frontend.domain_name}"
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.frontend.id
 }
